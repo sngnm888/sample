c2f815705b87f38d6fbfd22789ddf9e8d3106808
sngnm888 <150987777+sngnm888@users.noreply.github.com>
2023-11-16 12:36:20 +0900

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+window.onscroll = function() {stickyHeader()};
+
+var header = document.getElementById("site-header");
+var sticky = header.offsetTop;
+
+function stickyHeader() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+
+
